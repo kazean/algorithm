427fe4a80c68174418c97f45ba63a675eb71f0e7
yeogunna <yeogunna@gmail.com>
2023-09-16 18:25:28 +0900

    first commit

---
+++ src/main/java/programmers/JadenCaseSolution12951.java
@@ -0,0 +1,43 @@
+package programmers;
+
+public class JadenCaseSolution12951 {
+
+    public static void main(String[] args) {
+        JadenCaseSolution12951 solution12951 = new JadenCaseSolution12951();
+        System.out.println(solution12951.solution("3people unFollowed me"));
+        System.out.println(solution12951.solution("for the last week"));
+    }
+    public String solution(String s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char tmp = s.charAt(i);
+            char tmpResult;
+            if (' ' == tmp) {
+                flag = false;
+                sb.append(tmp);
+            } else {
+                if (!flag) {
+                    if ('a' <= tmp && 'z' >= tmp) {
+                        tmpResult = (char) (tmp - 32);
+                        sb.append(tmpResult);
+                    } else {
+                        sb.append(tmp);
+                    }
+                    flag = true;
+                } else {
+                    if ('A' <= tmp && 'Z' >= tmp) {
+                        tmpResult = (char) (tmp + 32);
+                        sb.append(tmpResult);
+                    } else {
+                        sb.append(tmp);
+                    }
+                }
+            }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
---
+++ src/main/java/programmers/Test.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class Test {
+    public static void main(String[] args) {
+        char a = 'a';
+        char A = 'A';
+        char z = 'z';
+        System.out.println((int)a);
+        System.out.println((int)A);
+        System.out.println((int)A - a);
+        System.out.println((char)(z - 32));
+    }
+}
