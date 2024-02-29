3a0a4cf382445207104942363992dbca4e97c34a
kazean <yeogunna@gmail.com>
2024-02-29 16:25:21 +0900

    practice basic1 - editor(Stack Ver)

--- src/main/java/baekjoon/basic1/datastructor/Stack_uses5_Main_1406_99_01.java
+++ src/main/java/baekjoon/basic1/datastructor/Stack_uses5_Main_1406_99_01.java
@@ -2,50 +2,58 @@ package baekjoon.basic1.datastructor;
 /*
 스택 - 에디터
  */
-import java.io.*;
-import java.util.LinkedList;
-import java.util.ListIterator;
 
-public class Stack_uses5_Main_1406 {
+import java.io.*;
+import java.util.Stack;
+
+public class Stack_uses5_Main_1406_99_01 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String initStr = br.readLine();
-        LinkedList<Character> list = new LinkedList<>();
+        int m = Integer.parseInt(br.readLine());
+
+        Stack<Character> leftS = new Stack<>();
+        Stack<Character> rightS = new Stack<>();
+
         for (int i = 0; i < initStr.length(); i++) {
-            list.add(initStr.charAt(i));
+            leftS.push(initStr.charAt(i));
         }
 
-        ListIterator<Character> iter = list.listIterator();
-        while (iter.hasNext()) {
-            iter.next();
-        }
-
-        int idx = list.size();
-
-        int cmdCnt = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < cmdCnt; i++) {
-            String s = br.readLine();
-            char c = s.charAt(0);
-            if ('P' == c) {
-                char t = s.charAt(2);
-                iter.add(t);
-            } else if ('L' == c) {
-                if(iter.hasPrevious()) iter.previous();
-            } else if ('D' == c) {
-                if(iter.hasNext()) iter.next();
-            } else if ('B' == c) {
-                if (iter.hasPrevious()) {
-                    iter.previous();
-                    iter.remove();
-                }
+        for (int i = 0; i < m; i++) {
+            String[] cmdArr = br.readLine().split(" ");
+            char cmd = cmdArr[0].charAt(0);
+            switch (cmd) {
+                case 'L':
+                    if (leftS.size() != 0) {
+                        rightS.push(leftS.pop());
+                    }
+                    break;
+                case 'D':
+                    if (rightS.size() != 0) {
+                        leftS.push(rightS.pop());
+                    }
+                    break;
+                case 'B':
+                    if (leftS.size() != 0) {
+                        leftS.pop();
+                    }
+                    break;
+                case 'P':
+                    char add = cmdArr[1].charAt(0);
+                    leftS.push(add);
+                    break;
             }
         }
 
-        for (Character chr : list) {
-            bw.write(chr);
+        while (!leftS.isEmpty()) {
+            rightS.push(leftS.pop());
         }
+        while (!rightS.isEmpty()) {
+            bw.write(rightS.pop());
+        }
+
         bw.flush();
         bw.close();
     }
